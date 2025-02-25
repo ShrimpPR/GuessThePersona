f740af2ec1dd0f81e679b6c394517be15859cd46
Jules Sourbets <jules.sourbets@gmail.com>
2025-02-25 14:33:50 +0100

    feat: add all login process

---
+++ front/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import AccountMenu from './AccountMenu';
+
+import './Header.css';
+
+const HeaderComponent = () => {
+    return (
+        <header>
+            <h1>my app</h1>
+            <div id="header-right">
+                <AccountMenu />
+            </div>
+        </header>
+    );
+};
+
+export default HeaderComponent;
