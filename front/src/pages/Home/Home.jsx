701136c96140561d9fb2a1ec398828b151f74af6
Periicles <landescrew.scoot@gmail.com>
2025-02-26 02:05:28 +0100

    refactor: updated the home page and it's style sheet

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,49 +1,68 @@
-import React from "react";
+/* eslint-disable react/no-unescaped-entities */
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import supabase from "../../helper/supabaseClient";
 import Button from '@mui/material/Button';
-
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 import "./Home.css";
-import { use } from "react";
 
 function Home() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			// Récupérer l'utilisateur via la nouvelle méthode getUser()
+			const { data: userData, error: userError } = await supabase.auth.getUser();
+			if (userError) {
+				console.error("Erreur récupération utilisateur :", userError.message);
+				return;
+			}
+
+			const user = userData?.user;
+			if (user) {
+				// S'il y a un utilisateur connecté, on redirige vers /chatbox
+				navigate("/chatbox");
+				return;
+			}
+		};
+
+		fetchUser();
+	}, [navigate]);
 
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      // Récupérer l'utilisateur via la nouvelle méthode getUser()
-      const { data: userData, error: userError } = await supabase.auth.getUser();
-      if (userError) {
-        console.error("Erreur récupération utilisateur :", userError.message);
-        return;
-      }
-
-      const user = userData?.user;
-      if (user) {
-        // S'il y a un utilisateur, on redirige vers /dashboard
-        navigate("/dashboard");
-        return;
-      }
-    };
-
-    fetchUser();
-  }, [navigate]);
-
-
-  return (
-    <div className="page-container">
-      <div className="log-reg-buttons">
-        <Button variant="outlined" href="/login" id="button">
-          Login
-        </Button>
-        <Button variant="outlined" href="/register" id="button">
-          Register
-        </Button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="page-container">
+			<link
+				href="https://fonts.googleapis.com/css2?family=Anton&family=Garet&display=swap"
+				rel="stylesheet"
+			/>
+			<div className="text-container">
+				<img
+					src="/LogoNoName.png"
+					alt="Guess the persona Logo"
+					className="logo"
+				/>
+				<p className="title-text">GUESS THE PERSONA</p>
+				<p className="subtitle-text-one">
+					L'HISTOIRE N'A JAMAIS ÉTÉ AUSSI AMUSANTE
+				</p>
+				<p className="subtitle-text-second">
+					EXPLORE LE PASSÉ, POSE DES QUESTIONS ET DEVINE LES FIGURES
+					HISTORIQUES QUI ONT MARQUÉ LE MONDE. PRÊT POUR LE DEFI ?
+				</p>
+				<div className="log-reg-buttons">
+					<Button
+						variant="outlined"
+						href="/login"
+						id="button"
+						endIcon={<PlayCircleOutlineIcon />}
+					>
+						JOUER
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
