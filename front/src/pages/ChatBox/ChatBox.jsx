638a9bf0fa49401988079abcff8c675ddbddf474
Shrimp <tristan.le-du@ouformer.com>
2025-02-26 01:06:22 +0100

    fix: chatbox display

--- front/src/pages/ChatBox/ChatBox.jsx
+++ front/src/pages/ChatBox/ChatBox.jsx
@@ -39,47 +39,47 @@ const ChatBox = () => {
         <div className={styles.menuItem}>Contact(temp)</div>
       </div>
 
-      <div className={styles.chatBoxContainer}>
-        <div className={styles.chatContainer}>
-          <div className={styles.chatBoxTitle}>DEVINE <p style={{ color: "#de97ff"}}>QUI JE SUIS</p></div>
-          <div className={styles.messagesContainer}>
-            {messages.map((msg, index) => (
-              <div key={index} className={`${styles.message} ${msg.sender === "user" ? styles.userMessage : styles.aiMessage}`}>
-                {msg.text}
-              </div>
-            ))}
-            {isTyping && <div className={styles.typingIndicator}>{typingDots}</div>}
-          </div>
-
-          <div className={styles.inputContainer}>
-            <input
-              className={styles.inputField}
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              placeholder="Pose ta question !"
-              onKeyUp={(e) => e.key === "Enter" && handleRequest({ type: "message", input, validationInput, setMessages, setInput, setValidationInput, setIsTyping, setIsBlurred })}
-            />
-            <button className={styles.sendButton} onClick={() => handleRequest({ type: "message", input, validationInput, setMessages, setInput, setValidationInput, setIsTyping, setIsBlurred })}>
-              <img src="/Icons/rightArrowIcon.svg" alt="Send" style={{ width: "2rem"}}/>
-            </button>
-          </div>
+      <div className={styles.chatContainer}>
+        <div className={styles.chatBoxTitle}>DEVINE <p style={{ color: "#de97ff"}}>QUI JE SUIS</p></div>
+        <div className={styles.messagesContainer}>
+          {messages.map((msg, index) => (
+            <div key={index} className={`${styles.message} ${msg.sender === "user" ? styles.userMessage : styles.aiMessage}`}>
+              {msg.text}
+            </div>
+          ))}
+          {isTyping && <div className={styles.typingIndicator}>{typingDots}</div>}
         </div>
 
-        <div className={styles.validationContainer}>
-          <img src="/circleBlur.svg" alt="" className={styles.validationCircle} />
-          <img src="https://picsum.photos/seed/picsum/450/450" alt="" className={`${styles.validationImage} ${isBlurred ? styles.blurred : ""}`} />
-          <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-            <input
-              className={styles.validationField}
-              value={validationInput}
-              onChange={(e) => setValidationInput(e.target.value)}
-              placeholder="Trouve son nom !"
-              onKeyUp={(e) => e.key === "Enter" && handleRequest({ type: "validate", input, validationInput, setMessages, setInput, setValidationInput, setIsTyping, setIsBlurred })}
-            />
-            <button className={styles.validationButton} onClick={() => handleRequest({ type: "validate", input, validationInput, setMessages, setInput, setValidationInput, setIsTyping, setIsBlurred })}>
-              <img src="/Icons/rightArrowIcon.svg" alt="Send" style={{ width: "2rem"}}/>
-            </button>
-          </div>
+        <div className={styles.inputContainer}>
+          <input
+            className={styles.inputField}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Pose ta question !"
+            onKeyUp={(e) => e.key === "Enter" && handleRequest({ type: "message", input, validationInput, setMessages, setInput, setValidationInput, setIsTyping, setIsBlurred })}
+          />
+          <button className={styles.sendButton} onClick={() => handleRequest({ type: "message", input, validationInput, setMessages, setInput, setValidationInput, setIsTyping, setIsBlurred })}>
+            <img src="/Icons/rightArrowIcon.svg" alt="Send" style={{ width: "2rem"}}/>
+          </button>
+        </div>
+      </div>
+
+      <div className={styles.separator}></div>
+
+      <div className={styles.validationContainer}>
+        <img src="/circleBlur.svg" alt="" className={styles.validationCircle} />
+        <img src="https://picsum.photos/seed/picsum/450/450" alt="" className={`${styles.validationImage} ${isBlurred ? styles.blurred : ""}`} />
+        <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+          <input
+            className={styles.validationField}
+            value={validationInput}
+            onChange={(e) => setValidationInput(e.target.value)}
+            placeholder="Trouve son nom !"
+            onKeyUp={(e) => e.key === "Enter" && handleRequest({ type: "validate", input, validationInput, setMessages, setInput, setValidationInput, setIsTyping, setIsBlurred })}
+          />
+          <button className={styles.validationButton} onClick={() => handleRequest({ type: "validate", input, validationInput, setMessages, setInput, setValidationInput, setIsTyping, setIsBlurred })}>
+            <img src="/Icons/rightArrowIcon.svg" alt="Send" style={{ width: "2rem"}}/>
+          </button>
         </div>
       </div>
     </div>
